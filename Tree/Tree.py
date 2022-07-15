4fab3a35822ed5cdf4c40d308522fe057e96bac5
dosmaster4life <jefferysuperman@gmail.com>
2022-07-15 14:21:18 -0600

    changed welcome to readme

--- Tree/Tree.py
+++ Tree/Tree.py
@@ -1,28 +1,103 @@
-class Stack:
-   def __init__(self):
-      self.stack = [] #create the stack(this is a list)
+class BST:
+    class Node:
 
-   def isEmpty(self): # Check if the stack is empty
-        if len(self.stack) > 0:
-            return False
+        def __init__(self, data):
+           
+            self.data = data
+            self.left = None
+            self.right = None
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if self.root is None:
+            self.root = BST.Node(data)
         else:
-            return True
+            self._insert(data, self.root)  
 
-   def push(self, value):
-      self.stack.append(value) # add an item to the stack
-        
+    def _insert(self, data, node):
+        if data < node.data:
+            if node.left is None:
+                node.left = BST.Node(data)
+            else:
+                self._insert(data, node.left)
+        elif data > node.data:
 
-   def pop(self):
-      if len(self.stack) > 0:
-         return self.stack.pop() # Use remove the top item from the stack
-      else:
-        return "stack is empty."
+            if node.right is None:
+                node.right = BST.Node(data)
+            else:
+
+                self._insert(data, node.right)
     
-   def peek(self): # view the top item of the stack
-    if not self.isEmpty():
-      print(self.stack[len(self.stack) - 1])
 
 
-   def printStack(self): # print all items in the stack, not native to a stack typically
-      for stackItem in reversed(self.stack):
-        print(stackItem)
+    def __contains__(self, data):
+        return self._contains(data, self.root) 
+
+
+    def _contains(self, data, node):
+        if node is None:
+           return False
+        else:
+            if data == node.data:
+                return True
+            if data < node.data:
+                return self._contains(data,node.left)
+            elif data > node.data:
+                return self._contains(data,node.right)
+        return False
+            
+
+    def __iter__(self):
+        yield from self._traverse_forward(self.root)  # Start at the root
+        
+    def _traverse_forward(self, node):
+      
+        if node is not None:
+            yield from self._traverse_forward(node.left)
+            yield node.data
+            yield from self._traverse_forward(node.right)
+        
+    def __reversed__(self):
+
+        yield from self._traverse_backward(self.root)
+
+    def _traverse_backward(self, node):
+
+        if node is not None:
+            yield from self._traverse_backward(node.right)
+            yield node.data
+            yield from self._traverse_backward(node.left)
+
+    def get_height(self):
+        if self.root is None:
+            return 0
+        else:
+            return self._get_height(self.root) 
+
+    def _get_height(self, node):
+      
+        if node is None:
+            return 0  
+        else:
+            return max(self._get_height(node.left),self._get_height(node.right)) + 1
+
+
+def create_bst_from_sorted_list(sorted_list):
+    bst = BST()  
+    _insert_middle(sorted_list, 0, len(sorted_list)-1, bst)
+    return bst
+
+def _insert_middle(sorted_list, first, last, bst):
+
+    if sorted_list :
+        middleOfList = (first + last)//2
+        bst.insert(sorted_list[middleOfList])
+        firstList = sorted_list[:middleOfList]
+        secondList = sorted_list[1+ middleOfList:]
+
+        _insert_middle(firstList, 0, len(firstList)-1, bst)
+        _insert_middle(secondList, 0, len(secondList)-1, bst)
+    return
+
